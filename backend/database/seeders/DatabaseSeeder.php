d5c93bf608fae47749c68e80a7f16879d8867e49
zjeffermari-a11y <zjeffermari@gmail.com>
2026-03-23 12:42:21 +0800

    Sync real user accounts

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'System Director',
-            'email' => 'director@dilg.gov.ph',
-            'password' => Hash::make('password'),
+            'name' => 'Lyra Zel Bedayo',
+            'email' => 'lzbedayo@gmail.com',
+            'password' => Hash::make('bedayo'),
             'role' => 'auditor',
             'designation' => 'director',
             'approval_status' => 'approved',
@@ -24,19 +24,29 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::create([
-            'name' => 'Division Chief',
-            'email' => 'chief@dilg.gov.ph',
-            'password' => Hash::make('password'),
+            'name' => 'Beyonce De Chavez',
+            'email' => 'btdechavez@gmail.com',
+            'password' => Hash::make('Luke24:15'),
             'role' => 'auditor',
             'designation' => 'division_chief',
             'approval_status' => 'approved',
             'agency_name' => 'DILG Central',
         ]);
+        
+        User::create([
+            'name' => 'Jean Antonette Tablizo',
+            'email' => 'jatablizo@gmail.com',
+            'password' => Hash::make('jeanantonette0601'),
+            'role' => 'auditor',
+            'designation' => 'assistant_division_chief',
+            'approval_status' => 'approved',
+            'agency_name' => 'DILG Central',
+        ]);
 
         User::create([
-            'name' => 'Lead Auditor',
-            'email' => 'admin@dilg.gov.ph',
-            'password' => Hash::make('password'),
+            'name' => 'Jeffer Mari Zepeda',
+            'email' => 'zjeffermari@gmail.com',
+            'password' => Hash::make('Z3ped@040500'),
             'role' => 'auditor',
             'designation' => 'lead_auditor',
             'approval_status' => 'approved',

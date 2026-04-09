91d26fb98c8cc7955fffcbdd4ea98a08a065d456
zjeffermari-a11y <zjeffermari@gmail.com>
2026-04-09 23:27:06 +0800

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -83,7 +83,7 @@ return [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT', '6543'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
